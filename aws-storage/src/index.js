08fb29344b6c2b036b1b1f1b3b0c0b1ba407ca69
andrei <and.korenevich@yandex.by>
2024-05-12 13:51:47 +0300

    feat aws-storage: add new microservice aws-storage

---
+++ aws-storage/src/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const AWS = require('aws-sdk');
+
+//
+// Throws an error if the any required environment variables are missing.
+//
+
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
+
+if (!process.env.STORAGE_BUCKET_NAME) {
+    throw new Error("Please specify the name of an AWS storage s3 bucket in environment variable STORAGE_BUCKET_NAME.");
+}
+
+if (!process.env.STORAGE_ACCESS_KEY_ID) {
+    throw new Error("Please specify the access key id to an AWS storage account in environment variable STORAGE_ACCESS_KEY_ID.");
+}
+
+if (!process.env.STORAGE_SECRET_ACCESS_KEY) {
+    throw new Error("Please specify the secret access key to an AWS storage account in environment variable STORAGE_SECRET_ACCESS_KEY.");
+}
+
+if (!process.env.STORAGE_REGION_NAME) {
+    throw new Error("Please specify the name of an AWS storage s3 bucket region in environment variable STORAGE_REGION_NAME.");
+}
+
+//
+// Extracts environment variables to globals for convenience.
+//
+
+const PORT = process.env.PORT;
+const STORAGE_BUCKET_NAME = process.env.STORAGE_BUCKET_NAME;
+const STORAGE_ACCESS_KEY_ID = process.env.STORAGE_ACCESS_KEY_ID;
+const STORAGE_SECRET_ACCESS_KEY = process.env.STORAGE_SECRET_ACCESS_KEY;
+const STORAGE_REGION_NAME = process.env.STORAGE_REGION_NAME;
+
+const app = express();
+
+// Configure AWS SDK with your credentials
+AWS.config.update({
+    accessKeyId: STORAGE_ACCESS_KEY_ID,
+    secretAccessKey: STORAGE_SECRET_ACCESS_KEY,
+    region: STORAGE_REGION_NAME
+});
+
+// Create an S3 instance
+const s3 = new AWS.S3();
+
+// Define a route to stream the video
+app.get('/video', (req, res) => {
+    const videoPath = req.query.path;
+    console.log(`Streaming video from path ${videoPath}.`);
+
+    // Create a read stream from S3 object
+    const stream = s3.getObject({ Bucket: STORAGE_BUCKET_NAME, Key: videoPath }).createReadStream();
+
+    // Set the appropriate content type for the video
+    res.setHeader('Content-Type', 'video/mp4');
+
+    // Pipe the stream to the response object
+    stream.pipe(res);
+});
+
+//
+// Starts the HTTP server.
+//
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
